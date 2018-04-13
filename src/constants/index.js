12dec5ee6f1d8c22b5f5ebef13b082c6413efe48
Flatiron School <leslie.hsu@flatironschool.com>
2018-04-13 13:19:37 -0400

    minor ws fox

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,5 +2,5 @@
 // export const API_WS_ROOT = 'ws://localhost:3001/cable'
 
 
-export const API_ROOT = 'https://letsget24backend.herokuapp.com/'
+export const API_ROOT = 'https://letsget24backend.herokuapp.com'
 export const API_WS_ROOT = 'wss://letsget24backend.herokuapp.com/cable'
