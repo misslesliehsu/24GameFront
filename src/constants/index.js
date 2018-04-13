d17661ae6a0e70f5446c8591ebcc7450921e949c
Flatiron School <leslie.hsu@flatironschool.com>
2018-04-13 00:07:35 -0400

    added double URLs

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 export const API_ROOT = 'http://localhost:3001/api/v1'
 export const API_WS_ROOT = 'ws://localhost:3001/cable'
-// export const HEADERS = {
-//   'Content-Type': 'application/json',
-//   Accept: 'application/json',
-// };
+
+
+export const API_ROOT = 'https://letsget24backend/api/v1'
+export const API_WS_ROOT = 'ws://letsget24backend/cable'
