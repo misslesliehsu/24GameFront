900977b57ba21858d355e964b6bb7e54bd288858
Flatiron School <leslie.hsu@flatironschool.com>
2018-04-13 12:52:19 -0400

    done

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
-export const API_ROOT = 'http://localhost:3001/api/v1'
-export const API_WS_ROOT = 'ws://localhost:3001/cable'
+// export const API_ROOT = 'http://localhost:3001/api/v1'
+// export const API_WS_ROOT = 'ws://localhost:3001/cable'
+//
 
-
-// export const API_ROOT = 'https://letsget24backend.herokuapp.com/api/v1'
-// export const API_WS_ROOT = 'ws://letsget24backend.herokuapp.com/cable'
+export const API_ROOT = 'https://letsget24backend.herokuapp.com/api/v1'
+export const API_WS_ROOT = 'ws://letsget24backend.herokuapp.com/cable'
